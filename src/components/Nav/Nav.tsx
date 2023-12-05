306582b4a61dc87fcb61d7683a362ee9795ce77a
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-12-05 12:00:21 +0900

    feat: 메인 Nav UI 구현

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,44 @@
+import {
+	HeaderContenstDiv,
+	HeaderDiv,
+	HeaderNav,
+	NavUl,
+	LogoH1,
+	NavLi,
+	UtilDl,
+	UtilDd,
+} from './Nav.style';
+import { IoChatbubblesOutline, IoNotificationsOutline } from 'react-icons/io5';
+import { HiOutlineUser } from 'react-icons/hi';
 const Nav = () => {
-	return <div>Nav</div>;
+	return (
+		<HeaderDiv>
+			<HeaderContenstDiv>
+				<HeaderNav>
+					<LogoH1>
+						<img src="assets/LOGO.svg" alt="" />
+					</LogoH1>
+					<NavUl>
+						<NavLi>새로운 가족 찾기</NavLi>
+						<NavLi>입양 후기</NavLi>
+						<NavLi>댕냥 톡톡</NavLi>
+					</NavUl>
+				</HeaderNav>
+				<UtilDl>
+					<dt></dt>
+					<UtilDd>
+						<IoChatbubblesOutline />
+					</UtilDd>
+					<UtilDd>
+						<IoNotificationsOutline />
+					</UtilDd>
+					<UtilDd>
+						<HiOutlineUser />
+					</UtilDd>
+				</UtilDl>
+			</HeaderContenstDiv>
+		</HeaderDiv>
+	);
 };
 
 export default Nav;
