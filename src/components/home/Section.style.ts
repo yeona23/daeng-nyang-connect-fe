719dbd94f0e1151716fe77b35ed4ddbf3e80f8be
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-12-06 14:31:24 +0900

    chore: 공통 사항 파일 변경

---
+++ src/components/home/Section.style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MainSection = styled.section`
+	max-width: 1320px;
+	padding: 80px 0 260px;
+	margin: auto;
+	text-align: center;
+`;
+export const MainSectionH2 = styled.h2`
+	font-size: 30px;
+	font-weight: 400;
+	margin-bottom: 80px;
+`;
