9cfb80950bc7e28d334f8c91eddfbeff18cc11ba
yeona23 <lachatte518@gmail.com>
2023-12-11 18:39:37 +0900

    feat: 입양후기 페이지 반응형 수정

--- src/components/adoptionReviews/Reviews.style.ts
+++ src/components/adoptionReviews/Reviews.style.ts
@@ -10,9 +10,10 @@ interface ResponsiveProps {
 //Reviews
 
 export const ReviewsContainer = styled.div<ResponsiveProps>`
-	width: ${(props) => (props.$isMaxWidth ? '1320px' : '100%')};
-	padding: ${(props) => (props.$isMaxWidth ? 'none' : '0 30px')};
-	margin: 0 auto;
+	width: ${(props) => (props.$isMaxWidth ? '100%' : '1320px')};
+	padding: ${(props) => (props.$isMaxWidth ? '0 30px' : '0')};
+	margin: ${(props) =>
+		props.$isMobile ? '0 auto 50px auto' : '120px  auto 150px auto'};
 `;
 
 export const ReviewsList = styled.div<ResponsiveProps>`
@@ -25,11 +26,16 @@ export const ReviewsList = styled.div<ResponsiveProps>`
 export const ReviewBox = styled.div<ResponsiveProps>`
 	display: flex;
 	flex-direction: column;
+	position: relative;
 	width: ${(props) =>
-		props.$isPc ? ' calc((100% - 100px) / 3)' : ' calc((100% - 30px) / 2)'};
-	aspect-ratio: 3 / 4.2;
-	margin-bottom: 50px;
-	border: 1px solid var(--color-deep-blue);
+		props.$isPc
+			? ' calc((100% - 100px) / 3)'
+			: props.$isTablet
+			  ? ' calc((100% - 30px) / 2)'
+			  : ' calc((100% - 20px) / 2)'};
+	aspect-ratio: ${(props) => (props.$isMobile ? 'unset' : '3 / 4.2')};
+	margin-bottom: ${(props) => (props.$isMobile ? '20px' : '50px')};
+	border: 1px solid var(--color-light-salmon);
 	border-radius: 10px;
 	overflow: hidden;
 
@@ -47,7 +53,7 @@ export const ReviewBox = styled.div<ResponsiveProps>`
 		flex-direction: column;
 		justify-content: space-between;
 		flex: 1;
-		padding: 20px;
+		padding: ${(props) => (props.$isMobile ? '10px' : '20px')};
 	}
 
 	& img {
@@ -59,6 +65,7 @@ export const ReviewBox = styled.div<ResponsiveProps>`
 		scale: 1.1;
 	}
 	& button {
+		display: ${(props) => (props.$isMobile ? 'none' : 'block')};
 		padding: 4px 25px;
 		border: 1px solid var(--color-light-salmon);
 		border-radius: 5px;
@@ -71,9 +78,10 @@ export const ReviewBox = styled.div<ResponsiveProps>`
 
 	& p {
 		display: -webkit-box;
-		margin-bottom: 15px;
+		margin-bottom: ${(props) => (props.$isMobile ? '5px' : '15px')};
+		font-size: ${(props) => (props.$isMobile ? '12px' : 'inherit')};
 		overflow-wrap: break-word;
-		-webkit-line-clamp: 4;
+		-webkit-line-clamp: ${(props) => (props.$isMobile ? '2' : '4')};
 		-webkit-box-orient: vertical;
 		text-overflow: ellipsis;
 		overflow: hidden;
@@ -81,6 +89,9 @@ export const ReviewBox = styled.div<ResponsiveProps>`
 	}
 
 	& svg {
+		position: ${(props) => (props.$isMobile ? 'absolute' : 'unset')};
+		top: 10px;
+		right: 10px;
 		cursor: pointer;
 	}
 `;
